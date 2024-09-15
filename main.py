1502f2fa4782922d59cb88693a83516250e7a41d
The-Quadragon <crsmith1991@gmail.com>
2024-09-14 22:39:32 -0600

    update to match code from example

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    char_count(text)
+    get_chars_dict(text)
 
 
 def get_num_words(text):
@@ -15,16 +15,14 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def char_count(text):
-    counts = {}
-    words = text.split()
-    for w in words:
-        lowered = w.lower()
-        for c in lowered:
-            if c in counts:
-                counts[c] += 1
-            else:
-                counts[c] = 1               
-    return counts
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
